97ec39a6f88b654b738adfd1b34249758894b9bd
MonkAscetic <monk_ascetic@163.com>
2025-01-14 11:46:54 +0800

    Site updated: 2025-01-14 11:46:53

---
+++ dist/music.js
@@ -0,0 +1,13 @@
+const ap = new APlayer({
+    container: document.getElementById('aplayer'),
+    fixed: true,
+    autoplay: true,
+    audio: [
+      {
+        name: "告白气球",
+        artist: '周杰伦',
+        url: 'https://github.com/MonkAscetic/music/raw/refs/heads/main/%E5%91%A8%E6%9D%B0%E4%BC%A6-%E5%91%8A%E7%99%BD%E6%B0%94%E7%90%83.mp3',
+        cover: 'https://www.bcmp3.com/zb_users/cache/thumbs/82bf6c7db874e17280d829796d82bee3-225-135-0.jpg',
+      }
+    ]
+});
