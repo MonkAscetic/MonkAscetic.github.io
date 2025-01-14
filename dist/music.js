d00c145dea3ab775208c9f13153ff1ed6159242d
MonkAscetic <monk_ascetic@163.com>
2025-01-14 18:41:48 +0800

    Site updated: 2025-01-14 18:41:47

--- dist/music.js
+++ dist/music.js
@@ -6,7 +6,13 @@ const ap = new APlayer({
       {
         name: "告白气球",
         artist: '周杰伦',
-        url: 'https://lw-sycdn.kuwo.cn/6a521a93e02e1406e0fa5d1e49423131/67860308/resource/30106/trackmedia/M500002QE4Dt4Gkrgd.mp3?bitrate$128&format$mp3',
+        url: 'https://raw.githubusercontent.com/MonkAscetic/music/refs/heads/main/%E5%91%A8%E6%9D%B0%E4%BC%A6/%E5%91%A8%E6%9D%B0%E4%BC%A6-%E5%91%8A%E7%99%BD%E6%B0%94%E7%90%83.mp3',
+        cover: 'https://www.bcmp3.com/zb_users/cache/thumbs/82bf6c7db874e17280d829796d82bee3-225-135-0.jpg',
+      },
+      {
+        name: "甜甜的",
+        artist: '周杰伦',
+        url: 'https://raw.githubusercontent.com/MonkAscetic/music/refs/heads/main/%E5%91%A8%E6%9D%B0%E4%BC%A6/%E5%91%A8%E6%9D%B0%E4%BC%A6-%E7%94%9C%E7%94%9C%E7%9A%84.flac',
         cover: 'https://www.bcmp3.com/zb_users/cache/thumbs/82bf6c7db874e17280d829796d82bee3-225-135-0.jpg',
       }
     ]
